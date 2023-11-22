4152f6be2bb068338d4badc670627d2d583b3052
suresh <sureshpradhana19@gmail.com>
2023-11-22 09:48:57 +0530

    1424

---
+++ 1424_Diagonal_traverse_2.js
@@ -0,0 +1,25 @@
+// group elements by the sum of row and column indices
+var findDiagonalOrder = function(nums) {
+    let output = {};
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        for (let j = 0; j < nums[i].length; j++) {
+            let diagonal = i + j;
+            if (!output[diagonal]) {
+                output[diagonal] = [];
+            }
+            output[diagonal].push(nums[i][j]);
+        }
+    }
+
+    let ans = [];
+    let curr = 0;
+
+    while (output[curr]) {
+        ans.push(...output[curr]);
+        curr++;
+    }
+
+    return ans;
+};
+
