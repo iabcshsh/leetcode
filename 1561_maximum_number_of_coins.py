7db10c9333865faa753ffe307cc19558d455255a
suresh <sureshpradhana19@gmail.com>
2023-11-24 15:53:01 +0530

    1561

---
+++ 1561_maximum_number_of_coins.py
@@ -0,0 +1,14 @@
+# Greedy simulation with sorting O(nlogn) time O(n) space
+def maxCoins( piles:list[int]) -> int:
+    length=len(piles)
+    choose=length//3
+    piles.sort()
+    re=0
+    while choose>0:
+        piles.pop()
+        re+=piles.pop()
+        choose-=1
+    print(re)
+    return re
+maxCoins([2,4,1,2,7,8])
+maxCoins([9,8,7,6,5,1,2,3,4]) 
